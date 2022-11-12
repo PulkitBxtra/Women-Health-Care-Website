e5f40a3f2bf1b7f0ea3c31455960493c0a1d6aab
Pulkit Batra <95132680+PulkitBxtra@users.noreply.github.com>
2022-11-12 22:47:59 +0530

    Create health.js

---
+++ health.js
@@ -0,0 +1,136 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
+
+    <title>Jumbotron Template for Bootstrap</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/jumbotron/">
+
+    <!-- Bootstrap core CSS -->
+    <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="jumbotron.css" rel="stylesheet">
+    <style>
+        .lower{
+  
+            display: flex;
+            justify-content: space-around;
+            margin-top:10vh;
+            margin-bottom:10vh;
+        }
+
+        .btn{
+            background-color: aliceblue;
+            color:black;
+        }
+
+    #Blank{
+        margin-top:30vh;
+    }
+
+
+    </style>
+  </head>
+
+  <body>
+
+  
+   
+      </div>
+    </nav>
+
+    <main role="main">
+
+       <!-- Main jumbotron for a primary marketing message or call to action --> 
+
+
+    <div id="Blank">
+
+    </div>
+
+
+    <div class="jumbotron">
+        <div class="container" >
+          <h1 class="display-3" style="height: 130px;"><b>Information about Healthcare</b></h1>
+          <p>Women's health includes a wide range of specialties and focus areas,
+             such as Birth control, sexually transmitted infections (STIs), and gynecology
+            Breast cancer, ovarian cancer, and other female cancers
+           ,Mammography, Menopause and hormone therapy , Osteoporosis , Pregnancy and childbirth ,Sexual health
+            Women and heart disease and Benign conditions affecting the function of the female reproductive organs.</p>
+          <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+        </div>
+      </div> 
+
+      <div id="Blank">
+
+      </div>
+
+
+      <div class="container">
+        <!-- Example row of columns -->
+        <div class="row">
+          <div class="col-md-4">
+            <h2>Physical Wellness</h2>
+            <p>Physical activity and a healthy, plant-based diet are key for heart disease and breast cancer prevention.</p>
+              <p>Regular practice helps women maintain better physical and mental health, energy levels, and self-esteem. It can also help with several medical conditions. </p>
+              
+             
+            
+          </div>
+          <div class="col-md-4">
+            <h2>Mental Wellness</h2>
+            <p>Mental wellness encompasses a balance of  spiritual and mental health. It’s about being aware of your emotions and knowing how to deal with stress and life’s changes.  </p>
+            <p> Life experiences can influence their vulnerability to emotional disturbances, and can affect decision making, and the ability to live life functionally. 
+
+            </p>
+           
+           
+          </div>
+          <div class="col-md-4">
+         
+            
+            <h2>Hygiene </h2>
+            
+            <p>Regular bathing is essential for everyone. The habit of skipping baths can lead to various skin infections such as dry skin and fungal infections.</p>
+            <p>Routine health checkup with your gynecologist is necessary to maintain health.</p>
+            <p></p><p></p>
+           
+           
+          </div>
+        </div>
+
+        <hr>
+
+      </div> <!-- /container -->
+
+
+    <div class="lower">
+     <div class="lowerleft"> <a class="btn btn-primary btn-lg" href="#" role="button">Meditation App</a></div>  
+       <div class="lowerright"> <a class="btn btn-primary btn-lg" href="#" role="button">BMI Calculator</a></div>
+    </div>
+
+    </main>
+
+    <footer class="container">
+      
+    </footer>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="../../assets/js/vendor/popper.min.js"></script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+  </body>
+</html>
